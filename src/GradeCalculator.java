c5b2fcb5776e264b9091bd32a56e5926e52a880d
raian621 <ryan.bell62101@gmail.com>
2022-06-17 23:57:41 -0500

    Revised GradeCalculator.java

--- src/GradeCalculator.java
+++ src/GradeCalculator.java
@@ -1,14 +1,6 @@
-import javax.swing.JFrame;
+public class GradeCalculator {
 
-public class GradeCalculator extends JFrame {
-
-    GradeCalculator() {
-        setSize(500, 500);
-        setVisible(true);
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-    }
-    
     public static void main(String[] args) {
-        GradeCalculator app = new GradeCalculator();
+        new PromptWindow();
     }
 }
